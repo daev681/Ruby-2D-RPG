1f6fbbc371545f6f8755d6b536c1c9664f1e83a2
daev681 <wow1186@naver.com>
2023-07-23 12:10:10 +0900

    이동구현 + 타일맵

--- Assets/2DBeginnerTutorialResources/Art/Scripts/RubyController.cs
+++ Assets/2DBeginnerTutorialResources/Art/Scripts/RubyController.cs
@@ -1,23 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class RubyController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-
+       // QualitySettings.vSyncCount = 0;
+       //  Application.targetFrameRate = 10;
     }
 
     // Update is called once per frame
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
-        float Vertical = Input.GetAxis("Vertical");
+        float vertical = Input.GetAxis("Vertical");
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f * horizontal;
-        position.y = position.y + 0.1f * Vertical;
+        position.x = position.x + 0.3f * horizontal * Time.deltaTime;
+        position.y = position.y + 0.3f * vertical * Time.deltaTime;
         transform.position = position;
     }
 }
