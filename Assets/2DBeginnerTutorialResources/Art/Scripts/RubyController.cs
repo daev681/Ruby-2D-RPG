8863d9a1faf046578748d05886689f723ccfc757
daev681 <wow1186@naver.com>
2023-07-30 19:46:28 +0900

    feat : 캐릭터 , 오브젝트 , 타일맵 충돌 추가

--- Assets/2DBeginnerTutorialResources/Art/Scripts/RubyController.cs
+++ Assets/2DBeginnerTutorialResources/Art/Scripts/RubyController.cs
@@ -5,11 +5,11 @@ using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class RubyController : MonoBehaviour
 {
+    Rigidbody2D rigidbody2d;
     // Start is called before the first frame update
     void Start()
     {
-       // QualitySettings.vSyncCount = 0;
-       //  Application.targetFrameRate = 10;
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -17,9 +17,13 @@ public class RubyController : MonoBehaviour
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-        Vector2 position = transform.position;
-        position.x = position.x + 0.3f * horizontal * Time.deltaTime;
-        position.y = position.y + 0.3f * vertical * Time.deltaTime;
-        transform.position = position;
+
+        // Vector2 position = transform.position;
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + 12.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 12.0f * vertical * Time.deltaTime;
+        //transform.position = position;
+        rigidbody2d.MovePosition(position); //  Rigidbody를 원하는 위치로 이동하고, 이동 중 다른 콜라이더와 충돌하면 멈춘다.
     }
+
 }
