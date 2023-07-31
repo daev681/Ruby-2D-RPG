456efa9464cdc93212400845e313cc1da43b106e
daev681 <wow1186@naver.com>
2023-07-31 20:14:33 +0900

    feat : maxHealth 구현

--- Assets/2DBeginnerTutorialResources/Art/Scripts/RubyController.cs
+++ Assets/2DBeginnerTutorialResources/Art/Scripts/RubyController.cs
@@ -1,15 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class RubyController : MonoBehaviour
 {
+    public float speed = 10.0f;
+
+    public int maxHealth = 5;
+    int currentHealth;
+
     Rigidbody2D rigidbody2d;
+
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
@@ -18,12 +22,16 @@ public class RubyController : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        // Vector2 position = transform.position;
         Vector2 position = rigidbody2d.position;
-        position.x = position.x + 12.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 12.0f * vertical * Time.deltaTime;
-        //transform.position = position;
-        rigidbody2d.MovePosition(position); //  Rigidbody를 원하는 위치로 이동하고, 이동 중 다른 콜라이더와 충돌하면 멈춘다.
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
+
+        rigidbody2d.MovePosition(position);
     }
 
+    void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + “/” +maxHealth);
+    }
 }
