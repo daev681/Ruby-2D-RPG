cb34c292fdfa42fa7b9d3b27cc51ece5582d221e
daev681 <wow1186@naver.com>
2023-08-01 16:46:40 +0900

    feat : enemy 애니메이터 구현 완료

--- Assets/2DBeginnerTutorialResources/Art/Scripts/EnemyController.cs
+++ Assets/2DBeginnerTutorialResources/Art/Scripts/EnemyController.cs
@@ -12,12 +12,15 @@ public class EnemyController : MonoBehaviour
     float timer;
     int direction = 1;
 
-    // Start is called before the first frame update
+    Animator animator;
+
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
         timer = changeTime;
+        animator = GetComponent<Animator>();
     }
+
 
     // Update is called once per frame
     void Update()
@@ -35,10 +38,14 @@ public class EnemyController : MonoBehaviour
 
         if (vertical)
         {
+            animator.SetFloat("Move X", 0);
+            animator.SetFloat("Move Y", direction);
             position.y = position.y + Time.deltaTime * speed * direction;
         }
         else
         {
+            animator.SetFloat("Move X", direction);
+            animator.SetFloat("Move Y", 0);
             position.x = position.x + Time.deltaTime * speed * direction;
         }
 
